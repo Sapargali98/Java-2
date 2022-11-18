8ff2dae4e5de66168b9a3178f0b0d309e6e04add
Aisha <aisha.sapargali@gmail.com>
2022-11-18 10:53:28 +0600

    first commit

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+public class Main {
+    public static void main(String[] args) {
+        /*
+        int a=10*5;
+        System.out.println(a);
+        double e=20/2.5;
+        System.out.println(e);
+        int a=15,b=9;
+        int S=a*b, P=(a+b)*2;
+        System.out.println(S);System.out.println(P);
+        int x=1;
+        x++;
+        System.out.println("x++:"+x);
+        ++x;
+        System.out.println("++x:"+x);
+        int x=3;
+        x+=5;
+        System.out.println(x);
+        int a=25,b=43;
+        System.out.println("a==b;"+(a==b));
+        int a=10,b=5;
+        int c=a+b,d=a*b,e=a-b,f=a/b;
+        System.out.println("a+b="+(c));
+        System.out.println("a*b="+(d));
+        System.out.println("a-b="+(e));
+        System.out.println("a/b="+(f));
+        int Far=1;
+        double C=(Far-32)/1.8;
+        System.out.println(C);
+        int c=10+451,d=c/3,f=d+5,e=f*5;
+        System.out.println(e);
+
+
+
+
+
+
+    }
+}
